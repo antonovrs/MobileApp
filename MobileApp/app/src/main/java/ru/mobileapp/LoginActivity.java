4f55acc05703849f752ef9bb87fd2f8bc276ab08
Andronina <kaeru.furan@yandex.ru>
2016-07-05 20:34:43 +0300

    change values login and password
    Изменены значения логина и пароля и вынесены в поля.

--- MobileApp/app/src/main/java/ru/mobileapp/LoginActivity.java
+++ MobileApp/app/src/main/java/ru/mobileapp/LoginActivity.java
@@ -4,37 +4,30 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+    private static final String LOGIN_VALUE = "madmin";
+    private static final String PASSWORD_VALUE = "q123456";
     Button singIn;
     EditText login, password;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        singIn =(Button)findViewById(R.id.singIn);
-        login =(EditText)findViewById(R.id.editLogin);
-        password =(EditText)findViewById(R.id.editPassword);
+        singIn = (Button) findViewById(R.id.singIn);
+        login = (EditText) findViewById(R.id.editLogin);
+        password = (EditText) findViewById(R.id.editPassword);
 
         singIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (login.getText().toString().equals("admin") &&
-
-                        password.getText().toString().equals("admin")) {
+                if (login.getText().toString().equals(LOGIN_VALUE) &&
+                        password.getText().toString().equals(PASSWORD_VALUE)) {
                     Toast.makeText(getApplicationContext(), "Загрузка...", Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
