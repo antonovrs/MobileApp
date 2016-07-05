a79c467ceca3a796f0caf59625de6dd8a82aa279
Andronina <kaeru.furan@yandex.ru>
2016-07-05 20:36:26 +0300

    refactoring

--- MobileApp/app/src/main/java/ru/mobileapp/FragmentTask.java
+++ MobileApp/app/src/main/java/ru/mobileapp/FragmentTask.java
@@ -1,9 +1,6 @@
 package ru.mobileapp;
 
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,100 +8,18 @@ import android.view.ViewGroup;
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragmentTask.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragmentTask#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentTask extends android.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public FragmentTask() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentTask.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentTask newInstance(String param1, String param2) {
-        FragmentTask fragment = new FragmentTask();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {View rootView =
-            inflater.inflate(R.layout.fragment_task, container, false);
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_task, container, false);
         PieChart mPieChart = (PieChart) rootView.findViewById(R.id.piechart);
 
-        mPieChart.addPieSlice(new PieModel("Выполнено", 35, Color.parseColor("#4CAF50")));
-        mPieChart.addPieSlice(new PieModel("В работе", 17, Color.parseColor("#FFC107")));
-        mPieChart.addPieSlice(new PieModel("Не выполнено", 35, Color.parseColor("#D8562A")));
-        mPieChart.addPieSlice(new PieModel("Создано", 38, Color.parseColor("#888888")));
-
+        mPieChart.addPieSlice(new PieModel(getResources().getString(R.string.task_done), 35, getResources().getColor(R.color.colorGreen)));
+        mPieChart.addPieSlice(new PieModel(getResources().getString(R.string.task_in_work), 17, getResources().getColor(R.color.colorYellow)));
+        mPieChart.addPieSlice(new PieModel(getResources().getString(R.string.task_not_done), 35, getResources().getColor(R.color.colorRed)));
+        mPieChart.addPieSlice(new PieModel(getResources().getString(R.string.task_create), 38, getResources().getColor(R.color.colorGray)));
         mPieChart.startAnimation();
-
         return rootView;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
