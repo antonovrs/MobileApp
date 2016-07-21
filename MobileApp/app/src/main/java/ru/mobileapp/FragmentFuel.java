d79b3b8fe8186ff45bdce80f02cc05c60f8ced14
antonovrs <antonovrs@mail.ru>
2016-07-21 09:58:12 +0300

    соответствие radioButton графику

--- MobileApp/app/src/main/java/ru/mobileapp/FragmentFuel.java
+++ MobileApp/app/src/main/java/ru/mobileapp/FragmentFuel.java
@@ -1,20 +1,29 @@
 package ru.mobileapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 public class FragmentFuel extends android.app.Fragment {
+
+    RadioGroup radioGroup;
 
     public void onStart() {
         super.onStart();
         getView().findViewById(R.id.sinkconsumption).callOnClick();
+        radioGroup.check(R.id.sinkconsumption);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fuel, container, false);
+        View root = inflater.inflate(R.layout.fragment_fuel, container, false);
+
+        radioGroup = (RadioGroup) root.findViewById(R.id.radioGroup);
+
+        return root;
     }
 }
